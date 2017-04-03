debf4f2bb0f272acda939d422ef52b80f09540e2
Nicolas Duchemin <duchemin.nicolas@gmail.com>
2017-04-03 14:19:35 +0900

    Progressing on the controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,53 @@
 class GameController < ApplicationController
-  # generate grid
-  # Start time
-  # end time
+
+  def run
+  end
+  
+  def score
+  end
+
+
+  def generate_grid(grid_size)
+    Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
+  end
+
+  def included?(guess, grid)
+    guess.split("").all? { |letter| grid.include? letter }
+  end
+
+  def compute_score(attempt, time_taken)
+    (time_taken > 60.0) ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    result = { time: end_time - start_time }
+
+    result[:translation] = get_translation(attempt)
+    result[:score], result[:message] = score_and_message(
+      attempt, result[:translation], grid, result[:time])
+
+    result
+  end
+
+  def score_and_message(attempt, translation, grid, time)
+    if translation
+      if included?(attempt.upcase, grid)
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not in the grid"]
+      end
+    else
+      [0, "not an english word"]
+    end
+  end
+
+
+  def get_translation(word)
+    response = open("http://api.wordreference.com/0.8/80143/json/enfr/#{word.downcase}")
+    json = JSON.parse(response.read.to_s)
+    json['term0']['PrincipalTranslations']['0']['FirstTranslation']['term'] unless json["Error"]
+  end
+
+end
 end
